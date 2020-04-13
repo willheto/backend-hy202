cc90b97cb6bbc1ba22636ceb28da9da5a2730f05
Henri Willman <60850956+willheto@users.noreply.github.com>
2020-04-13 13:31:39 +0300

    3.1

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+const port = 3001
+app.listen(port)
+console.log(`server running on port ${port}`)
+
+const numbers = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "040-123123123",
+        id: 2
+    },
+    {
+        name: "Dan abramov",
+        number: "12-43-234-345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    },
+]
+
+app.get('/api/persons', (request, response) => {
+    response.send(numbers)
+})
+
+app.get('/', (request, response) => {
+    response.send('')
+})
+
