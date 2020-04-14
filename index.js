4d56c9b878dd029ac7adcd15074b4fcbb1485059
Henri Willman <60850956+willheto@users.noreply.github.com>
2020-04-14 19:54:33 +0300

    build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 
 app.use(morgan(':method :url :response-time :type'))
